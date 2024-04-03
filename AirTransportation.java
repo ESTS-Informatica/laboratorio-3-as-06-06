95c443c750f97fcda41d642e136498928c44f5e9
Manuel Cantante <202002021@estudantes.ips.pt>
2024-04-03 11:06:28 +0100

    Nivel 2

--- AirTransportation.java
+++ AirTransportation.java
@@ -1,13 +1,43 @@
-
 public class AirTransportation extends Transport{
     private String name;
     private int numberOfContaineres;
-
-    public AirTransportation()
+    private int numberOfContainers;
+    public AirTransportation(String name, int numberOfContainers)
     {
         super();
         setFees(4.0);
 
+        if(name!= null){
+            this.name= name;
+        } else{
+            this.name = " ";
+        }
+
+        if(numberOfContainers>=0){
+            this.numberOfContainers = numberOfContainers;
+        } else {
+            this.numberOfContainers = 0;
+        }
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public void setName(String name){
+        if(name != null){
+            this.name = name;
+        }
+    }
+
+    public int getNumberOfContainers(){
+        return numberOfContainers;
+    }
+
+    public void setNumberOfContainers(int numberOfContainers){
+        if(numberOfContainers>=0){
+            this.numberOfContainers= numberOfContainers;
+        }
     }
 
 }
--- GroundTransportation.java
+++ GroundTransportation.java
@@ -2,10 +2,25 @@ public class GroundTransportation extends Transport{
     private String licensePlate;
 
 
-    public GroundTransportation()
+    public GroundTransportation(String licensePlate)
     {
         super();
         setFees(3.0);
+
+    if(licensePlate!= null){
+        this.licensePlate= licensePlate;
+    } else {
+        this.licensePlate= " ";
+    }
     }
 
+    public String getLicensePlate(){
+        return licensePlate;
+    }
+
+    public void setLicensePlate(String licensePlate){
+        if(licensePlate!= null){
+            this.licensePlate= licensePlate;
+        }
+    }
 }
