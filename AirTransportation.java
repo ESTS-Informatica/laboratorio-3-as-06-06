a008ba338da8abb23e8e3b80e85ef9842bcb2cd3
Manuel Cantante <202002021@estudantes.ips.pt>
2024-04-03 10:56:49 +0100

    Nivel 1

---
+++ AirTransportation.java
@@ -0,0 +1,13 @@
+
+public class AirTransportation extends Transport{
+    private String name;
+    private int numberOfContaineres;
+
+    public AirTransportation()
+    {
+        super();
+        setFees(4.0);
+
+    }
+
+}
---
+++ GroundTransportation.java
@@ -0,0 +1,11 @@
+public class GroundTransportation extends Transport{
+    private String licensePlate;
+
+
+    public GroundTransportation()
+    {
+        super();
+        setFees(3.0);
+    }
+
+}
